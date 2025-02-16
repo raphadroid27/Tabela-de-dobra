b04e17a282c8639261589a8948bf0efdc3bb510a
raphadroid27 <raphael.hs27@proton.me>
2025-02-16 16:54:11 -0300

    refatora a autenticação de usuário, cria funções de login, logout e requerimento de admin ou usuário, implementa a verificação de login na função de exclusão de canal

--- form_autenticacao.py
+++ form_autenticacao.py
@@ -5,42 +5,40 @@ from models import usuario
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 import hashlib
+from funcoes import *
 
 # Configuração do banco de dados
 engine = create_engine('sqlite:///tabela_de_dobra.db')
 Session = sessionmaker(bind=engine)
 session = Session()
-
-def autenticacao():
-    usuario_nome = usuario_entry.get()
-    usuario_senha = senha_entry.get()
     
-    usuario_obj = session.query(usuario).filter_by(nome=usuario_nome).first()
+def main(root_app):
+    g.aut_form = tk.Toplevel()
+    g.aut_form.geometry("300x200")
+    g.aut_form.resizable(False, False)
+    g.aut_form.title("Formulário de Autenticação")
+    #g.aut_form.attributes("-topmost", True)
 
-    if usuario_obj and usuario_obj.senha == hashlib.sha256(usuario_senha.encode()).hexdigest():
-        messagebox.showinfo("Login", "Login efetuado com sucesso.")
-        g.aut_form.destroy()
-    else:
-        messagebox.showerror("Erro", "Usuário ou senha incorretos.")
-    
-# Configuração da janela principal
-g.aut_form = tk.Tk()
-g.aut_form.geometry("300x200")
-g.aut_form.resizable(False, False)
-g.aut_form.title("Formulário de Autenticação")
+    # Posicionar a janela nova_deducao em relação à janela principal
+    g.aut_form.update_idletasks() 
+    x = root_app.winfo_x() + ((root_app.winfo_width() - g.aut_form.winfo_width()) // 2)
+    y = root_app.winfo_y() + ((root_app.winfo_height() - g.aut_form.winfo_height()) // 2)
+    g.aut_form.geometry(f"+{x}+{y}")
 
-# Rótulos e campos de entrada
-tk.Label(g.aut_form, text="Nome de usuário:").pack(pady=5)
-usuario_entry = tk.Entry(g.aut_form)
-usuario_entry.pack(pady=5)
+    tk.Label(g.aut_form, text="Nome de usuário:").pack(pady=5)
+    g.usuario_entry = tk.Entry(g.aut_form)
+    g.usuario_entry.pack(pady=5)
 
-tk.Label(g.aut_form, text="Senha:").pack(pady=5)
-senha_entry = tk.Entry(g.aut_form, show="*")
-senha_entry.pack(pady=5)
+    tk.Label(g.aut_form, text="Senha:").pack(pady=5)
+    g.senha_entry = tk.Entry(g.aut_form, show="*")
+    g.senha_entry.pack(pady=5)
 
-# Botão de login
-button_login = tk.Button(g.aut_form, text="Login", command=autenticacao)
-button_login.pack(pady=20)
+    # Botão de login
+    button_login = tk.Button(g.aut_form, text="Login", command=login)
+    button_login.pack(pady=20)
 
-# Iniciar o loop principal da interface gráfica
-g.aut_form.mainloop()
+    # Iniciar o loop principal da interface gráfica
+    g.aut_form.mainloop()
+
+if __name__ == "__main__":
+    main(None)
