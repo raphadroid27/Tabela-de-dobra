a76b6a60dd0b89faf64b857a420763dea64bc3a5
Raphael Henrique da Silva <raphael.hs27@proton.me>
2025-02-27 17:30:14 -0300

    implementa restaurar_dobras, refatora cálculos e atualiza interface
    - cria a função restaurar_dobras com validação
    - refatora a função e a lógica de cálculo de dobras e dobra_90
    - remove código de armazenamento/restauração de valores
    - atualiza a geometria da janela principal
    - adiciona verificação na função restaurar_dobras

--- dobra_90.py
+++ dobra_90.py
@@ -8,115 +8,58 @@ engine = create_engine('sqlite:///tabela_de_dobra.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 
-def dados_dobra(root):
-    dobra = ttk.Frame(root)
-    dobra.pack(padx=10, fill='both', expand=True)
+def form_dobra(frame, w):
+    g.frame_dobra = tk.Frame(frame)
+    g.frame_dobra.grid(row=0, column=0, sticky='we', pady=5)
 
-    # Novo frame para entradas de valores de dobra
-    frame_dobra = tk.Frame(dobra)
-    frame_dobra.pack(fill='both', expand=True, pady=5)
-
-    frame_dobra.columnconfigure(0, weight=1)
-    frame_dobra.columnconfigure(1, weight=1)
-    frame_dobra.columnconfigure(2, weight=1)
-    frame_dobra.columnconfigure(3, weight=1)
-
-    tk.Label(frame_dobra, text="Medida Ext.:").grid(row=0, column=1)
-    tk.Label(frame_dobra, text="Medida Dobra:").grid(row=0, column=2)
-    tk.Label(frame_dobra, text="Metade Dobra:").grid(row=0, column=3)
+    g.frame_dobra.columnconfigure(0, weight=1)
+    g.frame_dobra.columnconfigure(1, weight=1)
+    g.frame_dobra.columnconfigure(2, weight=1)
+    g.frame_dobra.columnconfigure(3, weight=1)
     
-    frame_dobra.rowconfigure(0, weight=0)
+    dobras(g.n, w)
+    return g.frame_dobra
+ 
+def dobras(valor, w):
+    # Atualizar o valor de n
+    g.n = valor
 
-    def dobras(valor):
-        g.n = valor
+    # Adicionar widgets novamente
+    tk.Label(g.frame_dobra, text="Medida Ext.:").grid(row=0, column=1)
+    tk.Label(g.frame_dobra, text="Medida Dobra:").grid(row=0, column=2)
+    tk.Label(g.frame_dobra, text="Metade Dobra:").grid(row=0, column=3)
 
-        # Armazenar os valores atuais
-        valores = {}
-        for i in range(1, g.n):
-            entry = getattr(g, f'aba{i}_entry', None)
-            if entry and entry.winfo_exists():
-                valores[f'aba{i}_entry'] = entry.get()
-            label = getattr(g, f'medidadobra{i}_label', None)
-            if label and label.winfo_exists():
-                valores[f'medidadobra{i}_label'] = label.cget("text")
-            label = getattr(g, f'metadedobra{i}_label', None)
-            if label and label.winfo_exists():
-                valores[f'metadedobra{i}_label'] = label.cget("text")
+    for i in range(1, g.n):
+        g.frame_dobra.rowconfigure(0, weight=0)
+        g.frame_dobra.rowconfigure(i, weight=0)
 
-        # Remover widgets existentes
-        for widget in frame_dobra.winfo_children():
-            widget.destroy()
+        tk.Label(g.frame_dobra, text=f"Aba {i}:").grid(row=i, column=0)
 
-        # Adicionar widgets novamente
-        tk.Label(frame_dobra, text="Medida Ext.:").grid(row=0, column=1)
-        tk.Label(frame_dobra, text="Medida Dobra:").grid(row=0, column=2)
-        tk.Label(frame_dobra, text="Metade Dobra:").grid(row=0, column=3)
+        setattr(g, f'aba{i}_entry_{w}', tk.Entry(g.frame_dobra, width=10))
+        entry = getattr(g, f'aba{i}_entry_{w}')
+        entry.grid(row=i, column=1, sticky='we', padx=2)
+        entry.bind("<KeyRelease>", lambda event: calcular_dobra(w))
+        
+        setattr(g, f'medidadobra{i}_label_{w}', tk.Label(g.frame_dobra, relief="sunken", width=10))
+        label = getattr(g, f'medidadobra{i}_label_{w}')
+        label.grid(row=i, column=2, sticky='we', padx=2)
+        label.bind("<Button-1>", lambda event, i=i: copiar('medida_dobra', i))
+        
+        setattr(g, f'metadedobra{i}_label_{w}', tk.Label(g.frame_dobra, relief="sunken", width=10))
+        label = getattr(g, f'metadedobra{i}_label_{w}')
+        label.grid(row=i, column=3, sticky='we', padx=2)
+        label.bind("<Button-1>", lambda event, i=i: copiar('metade_dobra', i))
 
-        for i in range(1, g.n):
-            frame_dobra.rowconfigure(i, weight=0)
+    tk.Label(g.frame_dobra, text="Medida do Blank:").grid(row=i+1, column=0, columnspan=2, sticky='e', padx=2)
 
-            tk.Label(frame_dobra, text=f"Aba {i}:").grid(row=i, column=0)
+    setattr(g, f'medida_blank_label_{w}', tk.Label(g.frame_dobra, relief="sunken", width=10))
+    medida_blank = getattr(g, f'medida_blank_label_{w}')
+    medida_blank.grid(row=i+1, column=2, sticky='we', padx=2)
+    medida_blank.bind("<Button-1>", lambda event: copiar('blank'))
+                      
+    setattr(g, f'metade_blank_label_{w}', tk.Label(g.frame_dobra, relief="sunken", width=10))
+    metade_blank = getattr(g,f'metade_blank_label_{w}')
+    metade_blank.grid(row=i+1, column=3, sticky='we', padx=2)
+    metade_blank.bind("<Button-1>", lambda event: copiar('metade_blank'))
 
-            setattr(g, f'aba{i}_entry', tk.Entry(frame_dobra, width=10))
-            entry = getattr(g, f'aba{i}_entry')
-            entry.grid(row=i, column=1, sticky='we', padx=2)
-            entry.bind("<KeyRelease>", lambda event: calcular_dobra())
-            # Restaurar valor
-            if f'aba{i}_entry' in valores:
-                entry.insert(0, valores[f'aba{i}_entry'])
 
-            setattr(g, f'medidadobra{i}_label', tk.Label(frame_dobra, relief="sunken", width=10))
-            label = getattr(g, f'medidadobra{i}_label')
-            label.grid(row=i, column=2, sticky='we', padx=2)
-            label.bind("<Button-1>", lambda event, i=i: copiar('medida_dobra', i))
-            # Restaurar valor
-            if f'medidadobra{i}_label' in valores:
-                label.config(text=valores[f'medidadobra{i}_label'])
-
-            setattr(g, f'metadedobra{i}_label', tk.Label(frame_dobra, relief="sunken", width=10))
-            label = getattr(g, f'metadedobra{i}_label')
-            label.grid(row=i, column=3, sticky='we', padx=2)
-            label.bind("<Button-1>", lambda event, i=i: copiar('metade_dobra', i))
-            # Restaurar valor
-            if f'metadedobra{i}_label' in valores:
-                label.config(text=valores[f'metadedobra{i}_label'])
-
-        tk.Label(frame_dobra, text="Medida do Blank:").grid(row=i+1, column=0, columnspan=2, sticky='e', padx=2)
-
-        g.medida_blank_label = tk.Label(frame_dobra, relief="sunken", width=10)
-        g.medida_blank_label.grid(row=i+1, column=2, sticky='we', padx=2)
-        g.medida_blank_label.bind("<Button-1>", lambda event: copiar('blank'))
-
-        g.metade_blank_label = tk.Label(frame_dobra, relief="sunken", width=10)
-        g.metade_blank_label.grid(row=i+1, column=3, sticky='we', padx=2)
-        g.metade_blank_label.bind("<Button-1>", lambda event: copiar('metade_blank'))
-
-        # Atualizar a geometria da janela principal
-        if g.n == 6:
-            g.principal_form.update_idletasks()
-            g.principal_form.geometry(f"340x400")
-        else:
-            g.principal_form.update_idletasks()
-            g.principal_form.geometry(f"340x500")
-
-    frame_inferior = tk.Frame(root)
-    frame_inferior.pack(fill='both', expand=True)
-
-    frame_inferior.columnconfigure(0, weight=1)
-    
-    estado = tk.StringVar(value="+")
-
-    def alternar_dobras():
-        if estado.get() == "+":
-            dobras(11)
-            estado.set("-")
-        else:
-            dobras(6)
-            estado.set("+")
-
-        calcular_dobra()
-
-    botao_alternar = tk.Button(frame_inferior, textvariable=estado, width=1, height=1, command=alternar_dobras)
-    botao_alternar.grid(row=0, column=0, sticky='we', padx=12)
-
-    dobras(g.n)
