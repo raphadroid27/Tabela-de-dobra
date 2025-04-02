1c3001b59972b32bd7091d13823f99064f0b444a
Raphael Henrique da Silva <raphael.hs27@proton.me>
2025-04-02 11:50:48 -0300

    adiciona navegação com teclas direcionais nos campos de entrada e ajusta a lógica de ordenação de materiais

--- form_aut.py
+++ form_aut.py
@@ -11,7 +11,7 @@ engine = create_engine('sqlite:///tabela_de_dobra.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 
-def main(root_app):
+def main(root):
     
     if g.aut_form is not None:
         g.aut_form.destroy()   
