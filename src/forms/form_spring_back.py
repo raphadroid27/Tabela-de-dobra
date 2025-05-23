f10ba78c6d4c76496f1b36643b659e7be88f9cfd
Raphael Henrique da Silva <raphael.hs27@proton.me>
2025-05-23 16:27:22 -0300

    refatora: corrige a consulta de materiais para garantir que os nomes sejam obtidos corretamente como strings

--- src/forms/form_spring_back.py
+++ src/forms/form_spring_back.py
@@ -15,8 +15,11 @@ spring_back_form.title("Cálculo de Spring Back")
 frame = tk.Frame(spring_back_form)
 frame.pack(pady=5, padx=5, fill='both', expand=True)
 
-tk.Label(frame, text = "Material:").grid(row=0, column=0)
-g.MAT_COMB = ttk.Combobox(frame, values = [session.query(Material)])
+# Obtém os nomes dos materiais como strings a partir da consulta
+materiais = [str(material.nome) for material in session.query(Material).all()]
+
+tk.Label(frame, text="Material:").grid(row=0, column=0)
+g.MAT_COMB = ttk.Combobox(frame, values=materiais)
 g.MAT_COMB.grid(row=1, column=0)
 
 spring_back_form.mainloop()
