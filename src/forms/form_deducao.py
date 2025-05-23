8f5f6dc97c4993743af3c4c8eaa526cc34d8c44c
Raphael Henrique da Silva <raphael.hs27@proton.me>
2025-05-23 15:41:42 -0300

    refatora: organiza a estrutura dos formulários, adiciona funções para configuração de frames e melhora a legibilidade do código

--- src/forms/form_deducao.py
+++ src/forms/form_deducao.py
@@ -10,7 +10,9 @@ from tkinter import ttk
 from src.utils.janelas import (no_topo, posicionar_janela)
 from src.utils.interface import (listar,
                                  limpar_busca,
-                                 atualizar_widgets
+                                 atualizar_widgets,
+                                 configurar_main_frame,
+                                 configurar_frame_edicoes
                                  )
 from src.utils.utilitarios import obter_caminho_icone
 from src.utils.operacoes_crud import (buscar,
@@ -21,11 +23,10 @@ from src.utils.operacoes_crud import (buscar,
                                       )
 from src.config import globals as g
 
-def main(root):
+def configurar_janela(root):
     '''
-    Função principal que inicializa e configura o formulário de deduções.
+    Configura a janela principal do formulário de deduções.
     '''
-    # Verificar se a janela já está aberta
     if g.DEDUC_FORM:
         g.DEDUC_FORM.destroy()
 
@@ -33,22 +34,16 @@ def main(root):
     g.DEDUC_FORM.geometry("500x420")
     g.DEDUC_FORM.resizable(False, False)
 
-    # Define o ícone
     icone_path = obter_caminho_icone()
     g.DEDUC_FORM.iconbitmap(icone_path)
 
     no_topo(g.DEDUC_FORM)
     posicionar_janela(g.DEDUC_FORM, None)
 
-    main_frame = tk.Frame(g.DEDUC_FORM)
-    main_frame.pack(pady=5, padx=5, fill='both', expand=True)
-
-    main_frame.columnconfigure(0, weight=1)
-
-    for i in [0, 2, 3]:
-        main_frame.rowconfigure(i, weight=0)
-    main_frame.rowconfigure(1, weight=1)
-
+def criar_frame_busca(main_frame):
+    '''
+    Cria o frame de busca.
+    '''
     frame_busca = tk.LabelFrame(main_frame, text='Buscar Deduções', pady=5)
     frame_busca.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
 
@@ -73,15 +68,21 @@ def main(root):
     tk.Button(frame_busca,
               text="Limpar",
               width=10,
-              command = lambda: limpar_busca('dedução')).grid(row=1, column=3, padx=5, pady=5)
+              command=lambda: limpar_busca('dedução')).grid(row=1, column=3, padx=5, pady=5)
 
-    columns = ("Id","Material", "Espessura","Canal", "Dedução", "Observação", "Força")
+def criar_lista_deducoes(main_frame):
+    '''
+    Cria a lista de deduções.
+    '''
+    columns = ("Id", "Material", "Espessura", "Canal", "Dedução", "Observação", "Força")
     g.LIST_DED = ttk.Treeview(main_frame, columns=columns, show="headings")
     g.LIST_DED["displaycolumns"] = ("Material",
-                                "Espessura",
-                                "Canal", "Dedução",
-                                "Observação",
-                                "Força")
+                                    "Espessura",
+                                    "Canal",
+                                    "Dedução",
+                                    "Observação",
+                                    "Força"
+                                    )
     for col in columns:
         g.LIST_DED.heading(col, text=col)
         g.LIST_DED.column(col, anchor="center", width=60)
@@ -89,28 +90,37 @@ def main(root):
         g.LIST_DED.column("Observação", width=120, anchor="w")
 
     g.LIST_DED.grid(row=1, column=0, padx=5, pady=5, sticky="ew")
-
     listar('dedução')
 
-    frame_edicoes = tk.LabelFrame(main_frame, pady=5)
-    frame_edicoes.grid(row=3, column=0, padx=5, pady=5, sticky="ew")
-
-    for i in range(4):
-        frame_edicoes.columnconfigure(i, weight=1)
+def criar_frame_edicoes(main_frame):
+    '''
+    Cria o frame de edições.
+    '''
+    frame_edicoes = configurar_frame_edicoes(main_frame,
+                                             text='Nova Dedução'
+                                             if not g.EDIT_DED
+                                             else 'Editar Dedução'
+                                             )
 
     tk.Label(frame_edicoes, text="Valor:").grid(row=0, column=0, padx=2, sticky='sw')
     g.DED_VALOR_ENTRY = tk.Entry(frame_edicoes)
     g.DED_VALOR_ENTRY.grid(row=1, column=0, padx=5, sticky="ew")
 
-    tk.Label(frame_edicoes, text="Observação:").grid(row=0, column=1,padx=2, sticky='sw')
+    tk.Label(frame_edicoes, text="Observação:").grid(row=0, column=1, padx=2, sticky='sw')
     g.DED_OBSER_ENTRY = tk.Entry(frame_edicoes)
     g.DED_OBSER_ENTRY.grid(row=1, column=1, padx=5, sticky="ew")
 
-    tk.Label(frame_edicoes, text="Força:").grid(row=0, padx=2, column=2, sticky='sw')
+    tk.Label(frame_edicoes, text="Força:").grid(row=0, column=2, padx=2, sticky='sw')
     g.DED_FORCA_ENTRY = tk.Entry(frame_edicoes)
     g.DED_FORCA_ENTRY.grid(row=1, column=2, padx=5, sticky="ew")
 
-    if g.EDIT_DED is True:
+    return frame_edicoes
+
+def configurar_botoes(main_frame, frame_edicoes):
+    '''
+    Configura os botões de ação (Adicionar, Atualizar, Excluir).
+    '''
+    if g.EDIT_DED:
         g.DEDUC_FORM.title("Editar/Excluir Dedução")
         g.LIST_DED.bind("<ButtonRelease-1>", lambda event: preencher_campos('dedução'))
         frame_edicoes.config(text='Editar Dedução')
@@ -119,20 +129,22 @@ def main(root):
                   text="Atualizar",
                   bg="green",
                   width=10,
-                  command = lambda: editar('dedução')).grid(row=1,
-                                                            column=3,
-                                                            padx=5,
-                                                            pady=5,
-                                                            sticky="eW")
+                  command=lambda: editar('dedução')).grid(row=1,
+                                                          column=3,
+                                                          padx=5,
+                                                          pady=5,
+                                                          sticky="eW"
+                                                          )
         tk.Button(main_frame,
                   text="Excluir",
                   bg="red",
                   width=10,
-                  command = lambda: excluir('dedução')).grid(row=2,
-                                                             column=0,
-                                                             padx=5,
-                                                             pady=5,
-                                                             sticky="e")
+                  command=lambda: excluir('dedução')).grid(row=2,
+                                                           column=0,
+                                                           padx=5,
+                                                           pady=5,
+                                                           sticky="e"
+                                                           )
     else:
         g.DEDUC_FORM.title("Adicionar Dedução")
         frame_edicoes.config(text='Nova Dedução')
@@ -140,15 +152,32 @@ def main(root):
                   text="Adicionar",
                   bg="cyan",
                   width=10,
-                  command = lambda: adicionar('dedução')).grid(row=1,
-                                                               column=3,
-                                                               padx=5,
-                                                               pady=5,
-                                                               sticky="eW")
+                  command=lambda: adicionar('dedução')).grid(row=1,
+                                                             column=3,
+                                                             padx=5,
+                                                             pady=5,
+                                                             sticky="eW"
+                                                             )
 
+def atualizar_comboboxes():
+    '''
+    Atualiza os widgets de combobox.
+    '''
     tipos = ['material', 'espessura', 'canal']
     for tipo in tipos:
         atualizar_widgets(tipo)
+
+def main(root):
+    '''
+    Inicializa e exibe o formulário de gerenciamento de deduções.
+    '''
+    configurar_janela(root)
+    main_frame = configurar_main_frame(g.DEDUC_FORM)
+    criar_frame_busca(main_frame)
+    criar_lista_deducoes(main_frame)
+    frame_edicoes = criar_frame_edicoes(main_frame)
+    configurar_botoes(main_frame, frame_edicoes)
+    atualizar_comboboxes()
 
 if __name__ == "__main__":
     main(None)
