b3241adb40a6633e2f1139d069c8c54eb76dbd1f
raphadroid27 <raphael.hs27@proton.me>
2025-06-01 20:03:35 -0300

    refatora: ajusta a função 'posicionar_janela' para usar 'app_principal' em vez de 'g.PRINC_FORM' e atualiza as funções de habilitar/desabilitar janelas

--- src/utils/janelas.py
+++ src/utils/janelas.py
@@ -14,15 +14,15 @@ def no_topo(form):
     on_top_valor = g.NO_TOPO_VAR.get() == 1
     set_topmost(form, on_top_valor)
 
-def posicionar_janela(form, posicao=None):
+def posicionar_janela(form, app_principal, posicao=None):
     '''
     Posiciona a janela em relação à janela principal.
     '''
     form.update_idletasks()
-    g.PRINC_FORM.update_idletasks()
-    largura_monitor = g.PRINC_FORM.winfo_screenwidth()
-    posicao_x = g.PRINC_FORM.winfo_x()
-    largura_janela = g.PRINC_FORM.winfo_width()
+    app_principal.janela_principal.update_idletasks()
+    largura_monitor = app_principal.janela_principal.winfo_screenwidth()
+    posicao_x = app_principal.janela_principal.winfo_x()
+    largura_janela = app_principal.janela_principal.winfo_width()
     largura_form = form.winfo_width()
 
     if posicao is None:
@@ -32,38 +32,38 @@ def posicionar_janela(form, posicao=None):
             posicao = 'direita'
 
     if posicao == 'centro':
-        x = g.PRINC_FORM.winfo_x() + ((g.PRINC_FORM.winfo_width() - largura_form) // 2)
-        y = g.PRINC_FORM.winfo_y() + ((g.PRINC_FORM.winfo_height() - form.winfo_height()) // 2)
+        x = app_principal.janela_principal.winfo_x() + ((app_principal.janela_principal.winfo_width() - largura_form) // 2)
+        y = app_principal.janela_principal.winfo_y() + ((app_principal.janela_principal.winfo_height() - form.winfo_height()) // 2)
     elif posicao == 'direita':
-        x = g.PRINC_FORM.winfo_x() + largura_janela + 10
-        y = g.PRINC_FORM.winfo_y()
+        x = app_principal.janela_principal.winfo_x() + largura_janela + 10
+        y = app_principal.janela_principal.winfo_y()
         if x + largura_form > largura_monitor:
-            x = g.PRINC_FORM.winfo_x() - largura_form - 10
+            x = app_principal.janela_principal.winfo_x() - largura_form - 10
     elif posicao == 'esquerda':
-        x = g.PRINC_FORM.winfo_x() - largura_form - 10
-        y = g.PRINC_FORM.winfo_y()
+        x = app_principal.janela_principal.winfo_x() - largura_form - 10
+        y = app_principal.janela_principal.winfo_y()
         if x < 0:
-            x = g.PRINC_FORM.winfo_x() + largura_janela + 10
+            x = app_principal.janela_principal.winfo_x() + largura_janela + 10
     else:
         return
 
     form.geometry(f"+{x}+{y}")
 
-def desabilitar_janelas():
+def desabilitar_janelas(app_principal):
     '''
     Desabilita todas as janelas do aplicativo.
     '''
-    forms = [g.PRINC_FORM, g.DEDUC_FORM, g.ESPES_FORM, g.MATER_FORM, g.CANAL_FORM]
+    forms = [app_principal.janela_principal, g.DEDUC_FORM, g.ESPES_FORM, g.MATER_FORM, g.CANAL_FORM]
     for form in forms:
         if form is not None and form.winfo_exists():
             form.attributes('-disabled', True)
             form.focus_force()
 
-def habilitar_janelas():
+def habilitar_janelas(app_principal):
     '''
     Habilita todas as janelas do aplicativo.
     '''
-    forms = [g.PRINC_FORM, g.DEDUC_FORM, g.ESPES_FORM, g.MATER_FORM, g.CANAL_FORM]
+    forms = [app_principal.janela_principal, g.DEDUC_FORM, g.ESPES_FORM, g.MATER_FORM, g.CANAL_FORM]
     for form in forms:
         if form is not None and form.winfo_exists():
             form.attributes('-disabled', False)
