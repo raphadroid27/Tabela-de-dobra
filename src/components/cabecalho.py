d83561e49ea09b589615a15a11db19562c27306b
raphadroid27 <raphael.hs27@proton.me>
2025-07-02 22:38:50 -0300

    organiza: centraliza tooltips do cabeçalho e implementa tooltip automático para combobox de canais

--- src/components/cabecalho.py
+++ src/components/cabecalho.py
@@ -7,7 +7,8 @@ from src.config import globals as g
 from src.utils.interface import (
     atualizar_widgets,
     calcular_valores,
-    copiar
+    copiar,
+    canal_tooltip
 )
 
 
@@ -32,7 +33,6 @@ def criar_widget(layout, tipo, nome_global, pos, **kwargs):
     """
     Cria e configura um widget, o adiciona ao layout e o armazena em g.
     """
-    justify = kwargs.pop('justify', None)
 
     if tipo == 'label':
         widget = QLabel(kwargs.pop('text', ''))
@@ -51,12 +51,7 @@ def criar_widget(layout, tipo, nome_global, pos, **kwargs):
     elif tipo == 'entry':
         widget = QLineEdit(**kwargs)
         widget.setFixedHeight(20)  # Altura fixa
-        if justify == 'center':
-            widget.setAlignment(Qt.AlignCenter)
-        elif justify == 'right':
-            widget.setAlignment(Qt.AlignRight)
-        else:
-            widget.setAlignment(Qt.AlignLeft)
+        widget.setAlignment(Qt.AlignCenter)  # Alinhamento padrão ao centro
     else:
         return None
 
@@ -88,13 +83,23 @@ def cabecalho(parent):
     criar_label(layout, "Canal", (0, 2))
     criar_label(layout, "Comprimento", (0, 3))
 
-
     # Widgets da Linha 1
     mat_comb = criar_widget(layout, 'combobox', 'MAT_COMB', (1, 0))
     esp_comb = criar_widget(layout, 'combobox', 'ESP_COMB', (1, 1))
     canal_comb = criar_widget(layout, 'combobox', 'CANAL_COMB', (1, 2))
-    compr_entry = criar_widget(layout, 'entry', 'COMPR_ENTRY', (1, 3), justify="center")
-    compr_entry.setToolTip("Digite o comprimento da peça em milímetros.")
+    compr_entry = criar_widget(layout, 'entry', 'COMPR_ENTRY', (1, 3))
+
+    # Tooltips para os widgets linha 1
+    mat_comb.setToolTip("Selecione o material.")
+    esp_comb.setToolTip("Selecione a espessura.")
+    canal_comb.setToolTip("Selecione o canal de dobra.")
+    compr_entry.setToolTip("Digite o comprimento.")
+
+    # Conectar eventos de mudança nas comboboxes
+    mat_comb.currentTextChanged.connect(calcular_valores)
+    esp_comb.currentTextChanged.connect(calcular_valores)
+    canal_comb.currentTextChanged.connect(calcular_valores)
+    compr_entry.textChanged.connect(calcular_valores)
 
 
     # Labels da Linha 2
@@ -103,16 +108,22 @@ def cabecalho(parent):
     criar_label(layout, "Dedução", (2, 2))
     criar_label(layout, "Offset", (2, 3))
 
-
     # Widgets da Linha 2
-    ri_entry = criar_widget(layout, 'entry', 'RI_ENTRY', (3, 0), justify="center")
-    ri_entry.textChanged.connect(calcular_valores)
-    ri_entry.setToolTip("Digite o raio interno da dobra em milímetros.")
+    ri_entry = criar_widget(layout, 'entry', 'RI_ENTRY', (3, 0))
     k_lbl = criar_widget(layout, 'label', 'K_LBL', (3, 1))
-    k_lbl.mousePressEvent = lambda event: copiar('fator_k')
     ded_lbl = criar_widget(layout, 'label', 'DED_LBL', (3, 2))
-    ded_lbl.mousePressEvent = lambda event: copiar('dedução')
     offset_lbl = criar_widget(layout, 'label', 'OFFSET_LBL', (3, 3))
+
+    # Tooltips para os widgets linha 2
+    ri_entry.setToolTip("Digite o raio interno em milímetros.")
+    k_lbl.setToolTip("Fator K calculado com base no raio interno.")
+    ded_lbl.setToolTip("Dedução de dobra.")
+    offset_lbl.setToolTip("Offset calculado com base no raio interno.")
+
+    # Conectar eventos de mudança nos widgets
+    ri_entry.textChanged.connect(calcular_valores)
+    k_lbl.mousePressEvent = lambda event: copiar('fator_k')
+    ded_lbl.mousePressEvent = lambda event: copiar('dedução')
     offset_lbl.mousePressEvent = lambda event: copiar('offset')
 
 
@@ -122,14 +133,20 @@ def cabecalho(parent):
     criar_label(layout, "Ext. Z90°", (4, 2))
     criar_label(layout, "Força", (4, 3))
 
-
     # Widgets da Linha 3
-    ded_espec_entry = criar_widget(layout, 'entry', 'DED_ESPEC_ENTRY', (5, 0), justify="center")
+    ded_espec_entry = criar_widget(layout, 'entry', 'DED_ESPEC_ENTRY', (5, 0))
+    aba_ext_lbl = criar_widget(layout, 'label', 'ABA_EXT_LBL', (5, 1))
+    z_ext_lbl = criar_widget(layout, 'label', 'Z_EXT_LBL', (5, 2))
+    forca_lbl = criar_widget(layout, 'label', 'FORCA_LBL', (5, 3))
+
+    #Tooltips para os widgets linha 3
+    ded_espec_entry.setToolTip("Digite a dedução específica em milímetros.")
+    aba_ext_lbl.setToolTip("Medida da aba mínima.")
+    z_ext_lbl.setToolTip("Medida de Z90° mínima.")
+    forca_lbl.setToolTip("Força necessária para a dobra.")
+
+    # Conectar eventos de mudança nos widgets
     ded_espec_entry.textChanged.connect(calcular_valores)
-    ded_espec_entry.setToolTip("Digite a dedução específica da peça em milímetros.")
-    criar_widget(layout, 'label', 'ABA_EXT_LBL', (5, 1))
-    criar_widget(layout, 'label', 'Z_EXT_LBL', (5, 2))
-    criar_widget(layout, 'label', 'FORCA_LBL', (5, 3))
 
 
     # Observações
@@ -138,6 +155,10 @@ def cabecalho(parent):
     obs_widget = criar_widget(layout, 'label', 'OBS_LBL', (7, 0))
     layout.addWidget(obs_widget, 7, 0, 1, 4)  # columnspan 4
 
+    # Tooltip para observações
+    obs_widget.setToolTip("Observações sobre a dobra ou material.")
+
+
     # Conectar eventos de mudança nas comboboxes após todos os widgets estarem criados
     if mat_comb:
         mat_comb.currentTextChanged.connect(lambda: atualizar_widgets('espessura'))
@@ -145,7 +166,11 @@ def cabecalho(parent):
         esp_comb.currentTextChanged.connect(lambda: atualizar_widgets('canal'))
     if canal_comb:
         canal_comb.currentTextChanged.connect(lambda: atualizar_widgets('dedução'))
+        canal_comb.currentTextChanged.connect(canal_tooltip)
     if compr_entry:
         compr_entry.textChanged.connect(calcular_valores)
+
+    # Configurar tooltip inicial do canal
+    canal_tooltip()
 
     return frame_cabecalho
