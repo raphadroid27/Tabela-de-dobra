5de8e882b42a661650161b7d7b1bf75adb48c160
raphadroid27 <raphael.hs27@proton.me>
2025-07-31 23:15:43 -0300

    refatora: remove variáveis globais não utilizadas e simplifica a definição de widgets

--- src/config/globals.py
+++ src/config/globals.py
@@ -3,7 +3,6 @@ Este módulo define variáveis globais para uso em diferentes partes da aplicaç
 """
 # Variáveis globais
 NO_TOPO_VAR = None
-TRANSP_VAR = None
 VALORES_W = [1, 2]
 EXP_V = None
 EXP_H = None
@@ -34,23 +33,14 @@ MAIN_LAYOUT = None
 MAT_COMB = None
 ESP_COMB = None
 CANAL_COMB = None
-DED_LBL = None
-RI_ENTRY = None
-K_LBL = None
 OFFSET_LBL = None
 OBS_LBL = None
-FORCA_LBL = None
 COMPR_ENTRY = None
-ABA_EXT_LBL = None
-Z_EXT_LBL = None
 
 # aba1.py widgets
-W = None
 N = 6
-for i in range(1, N):
-    globals()[f'aba{i}_entry_{W}'] = None
-    globals()[f'medidadobra{i}_label_{W}'] = None
-    globals()[f'metadedobra{i}_label_{W}'] = None
+# Widgets de dobra são criados dinamicamente:
+# aba{i}_entry_{w}, medidadobra{i}_label_{w}, metadedobra{i}_label_{w}
 DED_ESPEC_ENTRY = None
 FRAME_DOBRA = None
 
@@ -84,7 +74,6 @@ RAIO_K = {
 # nova_espessura.py widgets
 ESP_VALOR_ENTRY = None
 LIST_ESP = None
-EDIT_ESP = None
 ESP_BUSCA_ENTRY = None
 
 # deducao_form.py widgets
@@ -95,7 +84,6 @@ DED_VALOR_ENTRY = None
 DED_OBSER_ENTRY = None
 DED_FORCA_ENTRY = None
 LIST_DED = None
-EDIT_DED = None
 
 # material_form.py widgets
 MAT_NOME_ENTRY = None
@@ -103,7 +91,6 @@ MAT_DENS_ENTRY = None
 MAT_ESCO_ENTRY = None
 MAT_ELAS_ENTRY = None
 LIST_MAT = None
-EDIT_MAT = None
 MAT_BUSCA_ENTRY = None
 
 # canal_form.py widgets
@@ -113,19 +100,16 @@ CANAL_ALTUR_ENTRY = None
 CANAL_COMPR_ENTRY = None
 CANAL_OBSER_ENTRY = None
 LIST_CANAL = None
-EDIT_CANAL = None
 CANAL_BUSCA_ENTRY = None
 
 # form_autenticacao.py widgets
 USUARIO_ENTRY = None
 SENHA_ENTRY = None
 ADMIN_VAR = None
-LOGIN = None
 
 # form_usuario.py widgets
 USUARIO_BUSCA_ENTRY = None
 LIST_USUARIO = None
-EDIT_USUARIO = None
 
 # form_impressao.py widgets
 IMPRESSAO_FORM = None
