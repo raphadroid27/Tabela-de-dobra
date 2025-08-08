2924cebd1b12d10294612b5df0452eff4b0dddc6
raphadroid27 <raphael.hs27@proton.me>
2025-08-07 21:05:25 -0300

    refatora: ajusta a altura dos formulários de deduções, materiais e canais para melhor visualização

--- src/forms/form_universal.py
+++ src/forms/form_universal.py
@@ -44,7 +44,7 @@ from src.utils.utilitarios import ICON_PATH, aplicar_medida_borda_espaco
 FORM_CONFIGS = {
     "deducao": {
         "titulo": "Formulário de Deduções",
-        "size": (500, 500),
+        "size": (500, 510),
         "global_form": "DEDUC_FORM",
         "global_edit": "EDIT_DED",
         "busca": {
@@ -115,7 +115,7 @@ FORM_CONFIGS = {
     },
     "material": {
         "titulo": "Formulário de Materiais",
-        "size": (360, 500),
+        "size": (360, 510),
         "global_form": "MATER_FORM",
         "global_edit": "EDIT_MAT",
         "busca": {
@@ -168,7 +168,7 @@ FORM_CONFIGS = {
     },
     "canal": {
         "titulo": "Formulário de Canais",
-        "size": (360, 500),
+        "size": (360, 510),
         "global_form": "CANAL_FORM",
         "global_edit": "EDIT_CANAL",
         "busca": {
