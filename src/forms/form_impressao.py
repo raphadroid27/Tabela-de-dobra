7c89e99ea6e919152a1f4f937e2859cec3336801
Raphael Henrique da Silva <raphael.hs27@proton.me>
2025-08-20 14:05:16 -0300

    refatora: adiciona ícone ao botão de impressão para melhor visualização do estado

--- src/forms/form_impressao.py
+++ src/forms/form_impressao.py
@@ -527,7 +527,7 @@ class FormImpressao(QDialog):
                 return
 
             self.imprimir_btn.setEnabled(False)
-            self.imprimir_btn.setText("Imprimindo...")
+            self.imprimir_btn.setText("🖨️ Imprimindo...")
 
             self.print_worker = PrintWorker(
                 diretorio, self.print_manager.arquivos_encontrados
