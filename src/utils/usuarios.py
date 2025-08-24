4bad6932a35d3a4ebee4517e54ebc40282bdc461
raphadroid27 <raphael.hs27@proton.me>
2025-08-23 21:37:05 -0300

    refatora: melhora a formatação do título da janela principal no login

--- src/utils/usuarios.py
+++ src/utils/usuarios.py
@@ -103,7 +103,9 @@ def login():
             if g.AUTEN_FORM:
                 g.AUTEN_FORM.close()
             if g.PRINC_FORM:
-                titulo = f"Calculadora de Dobra - {getattr(usuario_obj, 'nome', 'Usuário')}"
+                titulo = (
+                    f"Calculadora de Dobra - {getattr(usuario_obj, 'nome', 'Usuário')}"
+                )
                 g.PRINC_FORM.setWindowTitle(titulo)
             if hasattr(g, "BARRA_TITULO") and g.BARRA_TITULO:
                 g.BARRA_TITULO.titulo.setText(titulo)
